d17ac2c0116800ce8d93c7f5fe1729a17603f833
Serhey1992 <makss92i@gmail.com>
2023-01-04 19:23:27 +0200

    done+/-

--- src/index.js
+++ src/index.js
@@ -18,15 +18,16 @@ function onCountryInput(){
        return clearHTML();
     }
     fetchCountries(name).then(country => {
+        clearHTML();
         if (country.length === 1) {
             createOneCountryMarkup(country);
         } else if(country.length >= 10){
-            Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
+           tooManyOptions();
         } else {
             createListOfCountryMarkup(country);
         };
     })
-        .catch(Notiflix.Notify.failure('Oops, there is no country with that name'));
+        .catch(wrongName);
 };
 
 
@@ -35,26 +36,35 @@ function clearHTML() {
     countryListEl.innerHTML = '';
 };
 
+function wrongName() {
+    Notiflix.Notify.failure('Oops, there is no country with that name')
+};
 
-function createOneCountryMarkup() {
+function tooManyOptions() {
+    Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.')
+};
+
+function createOneCountryMarkup(country) {
     const countryMarkup = country.map(({ name, flags, capital, population, languages }) => `
-    <li class="country-list__item">
-        <img class="country-list__item--flag" src="${flags.svg}" alt="flag of ${name.official}">
-        <h2 class="country-list__item--name">${name.official}</h2>
-        <h3 class="country-list__item--capital">Capital:${capital}</h3>
-        <p class="country-list__item--population">Population:${population}</p>
-        <p class="country-list__item--languages">Languages:${languages}</p>
-      </li>`)
+        <div class="country-list_conteiner">
+            <img class="country-list__item--flag" src="${flags.svg}" alt="flag of ${name.official}">
+            <h2 class="country-list__item--name">${name.official}</h2>
+        </div>
+        <h3 class="country-list__item--capital">Capital: ${capital}</h3>
+        <p class="country-list__item--population">Population: ${population}</p>
+        <p class="country-list__item--languages">Languages: ${Object.values(languages).join(', ')}</p>`)
     
     countryInfoEl.innerHTML = countryMarkup.join(" ");
 };
 
 
-function createListOfCountryMarkup() {
+function createListOfCountryMarkup(country) {
     const listMarkup = country.map(({ name, flags }) => `
         <li class="country-list__item">
-            <img class="country-list__item--flag" src="${flags.svg}" alt="flag of ${name.official}">
-            <h2 class="country-list__item--name">${name.official}</h2>
+            <div class="country-list_conteiner">
+                <img class="country-list__item--flag" src="${flags.svg}" alt="flag of ${name.official}">
+                <h2 class="country-list__item--name">${name.official}</h2>
+            </div>
         </li>`)
     
     countryListEl.innerHTML = listMarkup.join(" ");
